2a705b2c356fa90f3796834c6926948cfdc82b3c
aaronrmcgrath <armcgrathmedia@gmail.com>
2016-08-25 23:03:38 -0500

    update Reg and Login .cshtml files and models

--- WebApplication01_tasks/Models/AccountViewModels.cs
+++ WebApplication01_tasks/Models/AccountViewModels.cs
@@ -59,6 +59,10 @@ namespace WebApplication01_tasks.Models
 		public string Email { get; set; }
 
 		[Required]
+		[Display(Name = "FirstName")]
+		public string FirstName { get; set; }
+
+		[Required]
 		[DataType(DataType.Password)]
 		[Display(Name = "Password")]
 		public string Password { get; set; }
@@ -73,6 +77,10 @@ namespace WebApplication01_tasks.Models
 		[EmailAddress]
 		[Display(Name = "Email")]
 		public string Email { get; set; }
+
+		[Required]
+		[Display(Name = "FirstName")]
+		public string FirstName { get; set; }
 
 		[Required]
 		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
